e948f8f9f3628c60c24453af2414e275b192a508
anayib <nayib.abdala@gmail.com>
2015-01-06 14:36:57 -0500

    creates roles with enum in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,19 @@
 class User < ActiveRecord::Base
+  enum role: [:comerce, :customer, :admin]
+  after_initialize :set_default_role, :if => :new_record?
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :invoices
+  has_many :shops , through: :invoices
+
+   def set_default_role
+    self.role ||= :customer
+  end
+
+
 end
+
+
+
